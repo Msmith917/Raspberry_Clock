0ee52ea406c406f43cc1bc97713f04cf83a62f53
Michael Smith <wwe917@my.utsa.com>
2018-12-02 21:34:06 -0600

    Add files

---
+++ simple/digitalClock.py
@@ -0,0 +1,14 @@
+import time, tkinter
+from tkinter import *
+
+def tick():
+    time_string = time.strftime("%I:%M:%S %p")
+    clock.config(text=time_string)
+    clock.after(200,tick)
+
+root = tkinter.Tk()
+clock = Label(root, font=('times', 200))
+clock.grid(row=0, column=1)
+tick()
+
+root.mainloop()

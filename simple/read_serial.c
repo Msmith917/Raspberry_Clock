72d8fdfd41f726cf480bd3509fa150f7b9aa92e5
Michael Smith <wwe917@my.utsa.com>
2018-12-04 21:23:57 -0600

    changes to the arduino code. added the dht temp and humidity sensor

--- simple/read_serial.c
+++ simple/read_serial.c
@@ -1,7 +1,14 @@
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#define dht_pin 2
+#define DHTTYPE DHT22
+
 // initialize variables for the pin, and the floats, (celsius, and fahrenheit)
 float temp;
 int temp_pin = 2;
 float temp_f;
+
+DHT dht(dht_pin, DHTTYPE);
 
 void setup() {
   // begin the communication between devices
@@ -9,12 +16,21 @@ void setup() {
 }
 
 void loop() {
-  // convert the volts to fahrenheit
   temp = analogRead(temp_pin);
   temp_f = temp * .48828125;
   temp_f = temp_f * 9/5;
   temp_f = temp_f + 32;
-  // print the number to the serial monitor
-  Serial.println(temp_f);
+  Serial.print(temp_f);
+  Serial.print(" ");
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+
+  t = t * 9/5;
+  t = t + 32;
+  Serial.print(t);
+  Serial.print(" ");
+  Serial.print(h);
+  Serial.println("");
   delay(1000);
 }
+// Prints xx.x yy.y zz.z

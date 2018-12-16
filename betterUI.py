9ae6d04e4061949b9018cb4029a1c7adf021ee7e
Michael Smith <43732064+Msmith917@users.noreply.github.com>
2018-12-16 17:58:03 -0600

    Update betterUI.py

--- betterUI.py
+++ betterUI.py
@@ -20,11 +20,14 @@ current_date = Label(root, text="Today's Date")
 current_time = Label(root, text='Current Time', font=('times', 60))
 indoor_temp = Label(root, text='Indoor Temp')
 outdoor_temp = Button(root, text='Outdoor Temp', command=new_winF)
+jacket = Button(root, text='Do I Need A Jacket?')
 
 indoor_temp.pack(side='left', padx=30, pady=30)
 outdoor_temp.pack(side='right', padx=30, pady=30)
 
 current_date.pack(side='top', padx=30, pady=30)
 current_time.pack(anchor='center', padx=30, pady=100)
+
+jacket.pack(side='bottom', pady=100)
 
 root.mainloop()

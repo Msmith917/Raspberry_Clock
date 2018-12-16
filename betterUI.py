87b51fd48edaface6f4a79f1716e3abcdefab1ff
msmith917 <wwe917@my.utsa.com>
2018-12-16 17:53:13 -0600

    update to better ui

--- betterUI.py
+++ betterUI.py
@@ -1,33 +1,30 @@
 import tkinter
 from tkinter import *
+def new_winF(): # new window definition
+
+    newwin = Toplevel(root)
+
+    today_forecast = Label(newwin, text="Today's Forecast")
+    tomorrow_forecast = Label(newwin, text="Tomorrow's Forecast")
+    humidity = Label(newwin, text='Humidity')
+    outdoor_temp = Label(newwin, text='Outdoor Temp')
+
+    today_forecast.pack()
+    tomorrow_forecast.pack()
+    outdoor_temp.pack()
+    humidity.pack()
 
 root = tkinter.Tk()
 root.title('Pi_Clock')
 current_date = Label(root, text="Today's Date")
-today_forecast = Label(root, text="Today's Forecast")
-tomorrow_forecast = Label(root, text="Tomorrow's Forecast")
 current_time = Label(root, text='Current Time', font=('times', 60))
 indoor_temp = Label(root, text='Indoor Temp')
-outdoor_temp = Label(root, text='Outdoor Temp')
-humidity = Label(root, text='Humidity')
+outdoor_temp = Button(root, text='Outdoor Temp', command=new_winF)
 
-def new_winF(): # new window definition
-    newwin = Toplevel(root)
-    display = Label(newwin, text="Humm, see a new window !")
-    display.pack()
+indoor_temp.pack(side='left', padx=30, pady=30)
+outdoor_temp.pack(side='right', padx=30, pady=30)
 
-button1 =Button(root, text ="open new window", command =new_winF) #command linked
-
-indoor_temp.pack(side='left', side='top')
-outdoor_temp.pack(side='right', side='top')
-button1.pack()
-
-current_date.pack()
-today_forecast.pack()
-tomorrow_forecast.pack()
-current_time.pack()
-
-
-humidity.pack()
+current_date.pack(side='top', padx=30, pady=30)
+current_time.pack(anchor='center', padx=30, pady=100)
 
 root.mainloop()
